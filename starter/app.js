f4ef8a45430db3c1d5c178355488ad9cb68f7e6f
benconsult <bendayo38@gmail.com>
2024-02-19 08:02:14 +0100

    setup basic route

--- starter/app.js
+++ starter/app.js
@@ -1,1 +1,10 @@
-console.log('Task Manager App')
+const express = require('express')
+const app = express()
+
+app.get('/hello',(req,res)=>{
+    res.send('Task Manager')
+})
+const port = 3000
+app.listen(port, console.log(`server is running on port ${port}`))
+
+
